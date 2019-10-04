8fe17084e060ef0fe373d026ca82aba8415c5f4f
Ubuntu <ubuntu@ip-172-31-47-164.ec2.internal>
2019-10-04 04:09:07 +0000

    created function for hide/show page on click

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+/* global $ */
+
+$(function () {
+
+  $('.page').hide();
+  $('#page-one').show();
+
+  $(".nav-link").click(function () {
+    let page = $(this).data('dest');
+    $('.page').hide();
+    $('#' + page).show();
+  });
+})
