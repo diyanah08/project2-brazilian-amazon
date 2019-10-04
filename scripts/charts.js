1c113d52f81c32ad89590d786ec514c74a6c2cb7
Ubuntu <ubuntu@ip-172-31-47-164.ec2.internal>
2019-10-04 09:52:46 +0000

    added bar chart

--- scripts/charts.js
+++ scripts/charts.js
@@ -13,17 +13,32 @@ queue()
     let fire_per_state = state_dim.group().reduceSum(dc.pluck('number'));
 
       dc.pieChart('#pie-chart')
-        .height(500)
-        .radius(500)
-        .innerRadius(50)
-        .externalRadiusPadding(60)
+        .height(300)
+        .width(500)
+        .radius(150)
         .transitionDuration(1500)
-        .cx(250)
-        .cy(230)
         .dimension(state_dim)
         .group(fire_per_state)
         .colors(d3.scale.ordinal().range(["red", "orange", "green", "blue", "purple", "brown", "pink"]))
-        .legend(dc.legend().x(480).y(70).itemHeight(13).gap(5));
+        .legend(dc.legend().x(0).y(70).itemHeight(13).gap(5));
+        
+    let month_dim = fireCrossFilter.dimension(dc.pluck('month'));
+    let fire_per_month = month_dim.group().reduceSum(dc.pluck('number'));
+
+      dc.barChart("#bar-chart")
+        .width(700)
+        .height(450)
+        .margins({ top: 0, right: 50, bottom: 50, left: 100 })
+        .dimension(month_dim)
+        .group(fire_per_month)
+        .transitionDuration(250)
+        .x(d3.scale.ordinal())
+        .y(d3.scale.log().clamp(true).domain([1000, 550000]))
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Month")
+        .ordering(function(d) {return -d.value })
+        .yAxisLabel("Number of Fires")
+        .yAxis().ticks(20);
    
     dc.renderAll(); 
   });
